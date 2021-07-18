1a09d77f237ac897088141b96309e7db672be068
StefiLeon <stefaniapleon@gmail.com>
2021-07-18 10:43:33 -0300

    cambio logo

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ¡Bienvenidos a Tienda FutFem!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.futbolfemeninoargentino.com.ar"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Acceder al sitio
         </a>
       </header>
     </div>
